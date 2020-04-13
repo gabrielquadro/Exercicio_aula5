79b33de0843df928475877c904840e7575ede562
gabrielquadro <61526044+gabrielquadro@users.noreply.github.com>
2020-04-12 21:07:29 -0300

    Adção do menu, lista, spinner e evento ao escolher o estado e mostrar as cidades

--- app/src/main/java/com/unisc/exercicio_aula5/tela2.java
+++ app/src/main/java/com/unisc/exercicio_aula5/tela2.java
@@ -2,13 +2,43 @@ package com.unisc.exercicio_aula5;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ListActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
-public class tela2 extends AppCompatActivity {
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.List;
+
+public class tela2 extends ListActivity implements AdapterView.OnItemClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela2);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,listar());
+        setListAdapter(adapter);
+
+        ListView listView = getListView();
+        listView.setOnItemClickListener(this);
+    }
+
+    private List<String> listar(){
+        String[] array = {"Gabriel", "Carlos","Juca","Paula"};
+        return Arrays.asList(array);
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+        TextView textView = (TextView) view;
+        String nome = textView.getText().toString();
+        Intent intent = new Intent(this,tela3.class);
+        intent.putExtra("nome",nome);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/unisc/exercicio_aula5/tela3.java
+++ app/src/main/java/com/unisc/exercicio_aula5/tela3.java
@@ -3,12 +3,59 @@ package com.unisc.exercicio_aula5;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class tela3 extends AppCompatActivity {
 
+    private Spinner estados;
+    String[] estadosSul = {"Rio Grande do Sul","Santa Catarina","Paraná"};
+    private ListView listView;
+    String [] rs = {"Porto Alegre","Santa Cruz do Sul","Cachoeira do Sul"};
+    String [] sc = {"Florianópolis","Balneário Camboriú","Tubarão"};
+    String[] pr = {"Curitiba", "Londrina", "Maringá"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela3);
+        String nomePassado = getIntent().getStringExtra("nome");
+        TextView textView = findViewById(R.id.textview);
+        textView.setText("Nome: " + nomePassado);
+
+        listView = findViewById(R.id.listView);
+
+        estados = findViewById(R.id.estados);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,android.R.layout.simple_spinner_item, estadosSul);
+        estados.setAdapter(adapter);
+        estados.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getApplicationContext(), "Posiçao : " + position, Toast.LENGTH_SHORT).show();
+                switch (position){
+                    case 0:
+                        ArrayAdapter<String> adapter1 = new ArrayAdapter<>(getApplicationContext(), android.R.layout.simple_list_item_1, Arrays.asList(rs));
+                        listView.setAdapter(adapter1);
+                    case 1:
+                        ArrayAdapter<String> adapter2 = new ArrayAdapter<>(getApplicationContext(), android.R.layout.simple_list_item_1, Arrays.asList(sc));
+                        listView.setAdapter(adapter2);
+                    case 2:
+                        ArrayAdapter<String> adapter3 = new ArrayAdapter<>(getApplicationContext(), android.R.layout.simple_list_item_1, Arrays.asList(pr));
+                        listView.setAdapter(adapter3);
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
     }
 }
